b851dfca4a2b08198d76155d84cec3601dee4c43
Crownss <jesenkun@gmail.com>
2021-10-15 01:25:13 +0700

    completely comment and posting method function

--- Controllers/views.py
+++ Controllers/views.py
@@ -1,35 +1,27 @@
 from django.contrib.auth import login, logout
+from django.core.exceptions import BadRequest
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework import status
 from Models.models import *
-from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated, PermissionDenied
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated, NotFound, PermissionDenied
 from rest_framework.authtoken.models import Token
 from django.db.models import Q
+from Helpers import get_client_ip
 
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+
 
 class RegisterViewSet(ViewSet):
+
     def post(self, request):
         if request.user.is_authenticated:
             raise AuthenticationFailed('u already logged in !')
         serializer = UsersSerializers(data={'username':request.data['username'], 'email':request.data['email'], 'password':request.data['password'], 'verif_password':request.data['verif_password'], 'ip_address':get_client_ip(request)})
         if serializer.is_valid():
             if request.data['password'] != request.data['verif_password']:
-                return Response(
-                    {
-                        'message':"your password doesn't match"
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
-                )
+                raise BadRequest("password does'nt match !")
             serializer.save()
             return Response(
                 {
@@ -37,13 +29,10 @@ class RegisterViewSet(ViewSet):
                 },
                 status=status.HTTP_202_ACCEPTED
             )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+        raise BadRequest
 
 class LoginViewSet(ViewSet):
+
     def post(self, request):
         if request.user.is_authenticated:
             raise AuthenticationFailed('u already logged in !')
@@ -75,6 +64,7 @@ class LoginViewSet(ViewSet):
         return response
 
 class LogoutViewSet(ViewSet):
+
     def post(self, request):
         if request.user.is_authenticated:
             user = request.user
@@ -91,8 +81,17 @@ class LogoutViewSet(ViewSet):
         raise AuthenticationFailed('Cannot logged out, even u not logged in !')
 
 class PostingViewSet(ViewSet):
+
     def get(self, request):
-        pass
+        query = request.GET.get('postid')
+        if query:
+            posting_models = PostingModels.objects.filter(id=query)
+            if posting_models != None | 0:
+                return Response({'data':posting_models.values()},status=status.HTTP_200_OK)
+            raise NotFound
+        posting_models = PostingModels.objects.all()
+        return Response({'count':posting_models.count(), 'data':posting_models.values()},status=status.HTTP_200_OK)
+
     def post(self, request):
         if request.user.is_authenticated:
             user = request.user
@@ -103,11 +102,24 @@ class PostingViewSet(ViewSet):
             if posting_serializers.is_valid():
                 posting_serializers.save()
                 return Response({'message':'posted !'},status=status.HTTP_201_CREATED)
-            return Response({'message':posting_serializers.error_messages},status=status.HTTP_400_BAD_REQUEST)
+            raise BadRequest('something wrong with your request !')
         raise NotAuthenticated('loggin first !')
+
     def update(self, request):
         if request.user.is_authenticated:
             user = request.user
+            query = request.GET.get('postid')
+            if query:
+                admin_check = User.objects.filter(username=user.username).first()
+                if admin_check.is_superuser != True:
+                    raise PermissionDenied
+                posting_models = PostingModels.objects.filter(id=query)
+                posting_serializers = PostingSerializers
+                if posting_models.count() != 0:
+                    posting_serializers.update(self, instance=posting_models, validated_data={'author':user.username, 'img':request.data['img'], 'title':request.data['title'], 'desc':request.data['desc'], 'description':request.data['description'], 'link':request.data['link']})
+                raise NotFound
+        raise NotAuthenticated('loggin first !')
+
     def delete(self, request):
         if request.user.is_authenticated:
             user = request.user
@@ -119,16 +131,54 @@ class PostingViewSet(ViewSet):
                 posting_models = PostingModels.objects.filter(id=query)
                 if posting_models != None & 0:
                     return Response({'data':posting_models.values().first()},status=status.HTTP_200_OK)
-                raise Response({'message':'not found any data with id: '+ query},status=status.HTTP_404_NOT_FOUND)
+                raise NotFound('not found any data with id: '+ query)
         raise NotAuthenticated('loggin first !')
 
 
 class CommentViewSet(ViewSet):
+
     def get(self, request):
-        pass
+        query = request.GET.get('postid')
+        if query:
+            comment_models = CommentModels.objects.filter(toPost=query)
+            if comment_models.count != 0:
+                return Response({'count':comment_models.count(), 'data':comment_models.values(), 'message':'found !'},status=status.HTTP_302_FOUND)
+            raise NotFound
+        comment_models = CommentModels.objects.all()
+        return Response({'count':comment_models.count(), 'data':comment_models.values(),'message':'found all commentar !'},status=status.HTTP_200_OK)
+    
     def post(self, request):
-        pass
+        query = request.GET.get('postid')
+        if query:
+            posting_models = PostingModels.objects.get(id=query)
+            comment_serializers = CommentSerializers(data={'username':request.data['username'], 'comment':request.data['comment'], 'toPost':posting_models, 'ip_address':get_client_ip(request)})
+            if comment_serializers.is_valid():
+                comment_serializers.save()
+                return Response({'message':'success comment !'},status=status.HTTP_201_CREATED)
+            raise BadRequest('something wrong with your request !')
+        # comment_serializers = CommentSerializers(data={'username':request.data['username'], 'comment':request.data['comment'], 'toPost':None, 'ip_address':get_client_ip(request)})
+        # if comment_serializers.is_valid():
+        #     comment_serializers.save()
+        #     return Response({'message':'success comment !'},status=status.HTTP_202_ACCEPTED)
+        # return Response({'message':'something wrong with your request !'},status=status.HTTP_400_BAD_REQUEST)
+
     def update(self, request):
-        pass
+        query = request.GET.get('postid')
+        if query:
+            comment_models = CommentModels.objects.filter(toPost=query, ip_address=get_client_ip(request))
+            comment_serializers = CommentSerializers
+            if comment_models.count() != 0:
+                comment_serializers.update(self, instance=get_client_ip(request), validated_data={'username':request.data['username'], 'comment':request.data['comment'], 'toPost':comment_models, 'ip_address':get_client_ip(request)})
+                return Response({'message':'updated your comment !'},status=status.HTTP_202_ACCEPTED)
+            raise NotFound
+        raise NotFound
+
     def delete(self, request):
-        pass
+        query = request.GET.get('postid')
+        if query:
+            comment_models = CommentModels.objects.filter(toPost=query, ip_address=get_client_ip(request))
+            if comment_models.count() != 0:
+                comment_models.delete()
+                return Response({'message':'deleted !'})
+            raise NotFound
+        raise NotFound
